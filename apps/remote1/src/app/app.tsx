0320eaa56f476eff99c086b2a85aec7dffc1973a
zmzlois <lois.z@comcord.vision>
2024-10-23 22:49:52 +0100

    update

---
+++ apps/remote1/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from './nx-welcome';
+
+import '../styles.css';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="remote1" />
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/remote2/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from './nx-welcome';
+
+import '../styles.css';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="remote2" />
+    </div>
+  );
+}
+
+export default App;
